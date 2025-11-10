7262a2e634b2ebdbe97fc99562605909d894e45f
emna404 <emna.driss@esprit.tn>
2025-11-10 11:27:25 +0100

    checkin 1 red

--- src/test/java/PrimeCheckerTest.java
+++ src/test/java/PrimeCheckerTest.java
@@ -1,10 +1,16 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PrimeCheckerTest {
     PrimeChecker PrimeChecker = new PrimeChecker();
-    @Test
+
    public void test0IsNotPrime(){
          assertFalse(PrimeChecker.isPrime(0), "0 should not be prime");
+   }
+    @Test
+   public void test1IsPrime(){
+         assertTrue(PrimeChecker.isPrime(1), "1 should be prime");
    }
 
 }
