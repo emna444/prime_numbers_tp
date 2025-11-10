5d7b418b6e93d5339502d312c2a8aedf978eb4e8
emna404 <emna.driss@esprit.tn>
2025-11-10 11:50:47 +0100

    checkin 1 blue

--- src/main/java/PrimeChecker.java
+++ src/main/java/PrimeChecker.java
@@ -1,10 +1,6 @@
 public class PrimeChecker {
     public boolean isPrime(int number) {
 
-        if (number == 1) {
-            return true;
-        }
-
-return false;
+        return number == 1;
     }
 }
